08292ce85736299bcc085f7241811a64a5803f67
tomastrg <tomastrg22@gmail.com>
2019-05-25 18:53:05 +0200

    add web3

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 // import { useAragonApi } from '@aragon/api-react'
 import { Main, TabBar } from '@aragon/ui'
 import PartyList from './components/PartyList'
 import CreateParty from './components/CreateParty'
 import CreateProgram from './components/CreateProgram'
+import Web3 from 'web3'
 
-function App() {
-  // const { api, appState } = useAragonApi()
-  // const { count, syncing } = appState
-  const [selected, setSelected] = useState(0)
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      account: window.web3.eth.coinbase,
+      web3: window.web3,
+      ethereum: window.ethereum,
+      selected: 0,
+      web3Provider: window.ethereum ? new Web3(window.ethereum) : null,
+    }
+    console.log(this.state.web3Provider)
 
-  const renderSelectedPage = pageNumber => {
+  }
+
+  setSelected = selectedTab => {
+    this.setState({ selected: selectedTab })
+  }
+
+  selectPage = pageNumber => {
     if (pageNumber === 0) {
       return <PartyList />
     } else if (pageNumber === 1) {
@@ -20,16 +34,23 @@ function App() {
     }
   }
 
-  return (
-    <Main>
-      <TabBar
-        items={['Parties', 'Create Party', 'CreateProgram']}
-        selected={selected}
-        onChange={setSelected}
-      />
-      {renderSelectedPage(selected)}
-    </Main>
-  )
+  render() {
+    if (this.state.web3 && this.state.account) {
+      console.log('metamask enabled', this.state.account)
+    } else {
+      console.log('metamask not enabled')
+    }
+    return (
+      <Main>
+        <TabBar
+          items={['Parties', 'Create Party', 'CreateProgram']}
+          selected={this.selected}
+          onChange={this.setSelected}
+        />
+        {this.selectPage(this.state.selected)}
+      </Main>
+    )
+  }
 }
 
 export default App
